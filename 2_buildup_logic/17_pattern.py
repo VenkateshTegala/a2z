31fcc01daea6c47ffdacdb7cd37ba97b17fe6e30
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-03-10 23:55:17 +0530

    Added new file

---
+++ 2_buildup_logic/17_pattern.py
@@ -0,0 +1,30 @@
+'''
+*                   *
+* *               * *
+* * *           * * *
+* * * *       * * * *
+* * * * *   * * * * *
+* * * * *   * * * * * 
+* * * *       * * * *
+* * *           * * *
+* *               * *
+*                   * 
+'''
+
+n = int(input())
+for i in range(n):
+    for j in range(i + 1):
+        print("*", end=" ")
+    for j in range(i + 1, n + n - i):
+        print(" ", end=" ")
+    for j in range(n + n - i, n + n + 1):
+        print("*", end=" ")
+    print()
+for i in range(n - 1, -1 , -1):
+    for j in range(i + 1):
+        print("*", end=" ")
+    for j in range(i + 1, n + n - i):
+        print(" ", end=" ")
+    for j in range(n + n - i, n + n + 1):
+        print("*", end=" ")
+    print()
