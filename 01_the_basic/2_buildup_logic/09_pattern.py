fed824e508eacca92b880e762292e9207ff16eb9
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-03-12 16:55:00 +0530

    Added new file

--- 01_the_basic/2_buildup_logic/09_pattern.py
+++ 01_the_basic/2_buildup_logic/09_pattern.py
@@ -23,7 +23,7 @@ for i in range(n):
         print(" ", end=" ")
     print()
     
-for i in range(n - 1, -1, -1):
+for i in range(n - 2, -1, -1):
     for j in range(n - i - 1):
         print(" ", end=" ")
     for j in range(n - i - 1, n + i):
