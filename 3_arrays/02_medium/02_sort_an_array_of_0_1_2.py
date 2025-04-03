33c69f40b4fddca2ef38d83c4fda51a63deb9546
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-04-03 22:48:03 +0530

    Added new file

---
+++ 3_arrays/02_medium/02_sort_an_array_of_0_1_2.py
@@ -0,0 +1,43 @@
+'''
+    Dutch National Flag Algorithm
+
+75. Sort Colors
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+You must solve this problem without using the library's sort function.
+
+Example 1:
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Example 2:
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Constraints:
+n == nums.length
+1 <= n <= 300
+nums[i] is either 0, 1, or 2.
+
+Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+'''
+
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        low = 0
+        middle = 0
+        high = len(nums) - 1
+        while middle <= high:
+            if nums[middle] == 0:
+                nums[low], nums[middle] = nums[middle], nums[low]
+                low += 1
+                middle += 1
+            elif nums[middle] == 1:
+                middle += 1
+            else:
+                nums[high], nums[middle] = nums[middle], nums[high]
+                high -= 1
