b596160e12054767523ccc4698a7080d2ead1957
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-06-11 20:45:05 +0530

    Added new file

--- 3_arrays/02_medium/10_set_matrix_zeroes.py
+++ 3_arrays/02_medium/10_set_matrix_zeroes.py
@@ -62,4 +62,34 @@ class Solution(object):
             for j in range(len(matrix[0])):
                 if row_list[i] == 1 or col_list[j] == 1:
                     matrix[i][j] = 0
+                    
+#Optimal : Without extra space
+#By considering the first row and first column to store
+#Time complexity: O(2*n^2)  Space complexity: O(1) (one variable)
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        j0 = 1
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    if j != 0:
+                        matrix[0][j] = 0
+                    else:
+                        j0 = 0
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
+                    matrix[i][j] = 0
+        if matrix[0][0] == 0:
+            for j in range(len(matrix[0])):
+                matrix[0][j] = 0
+        if j0 == 0:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
+        
 
